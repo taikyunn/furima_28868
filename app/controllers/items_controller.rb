a45b1d0cb0ff8e290c2332dfbfe5f34bf5772e21
taikyunn <taichi_141@me.com>
2020-09-04 11:29:32 +0900

    完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :set_params, only: [:show]
+  before_action :set_params, only: [:show]
 
   def index
     @items = Item.all.order('created_at DESC')

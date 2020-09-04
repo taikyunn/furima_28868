ec3aab7261b085a290a32e4736f3f1aba4fe1601
taikyunn <taichi_141@me.com>
2020-09-04 16:16:36 +0900

    コントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_params, only: %i[show edit update]
+  before_action :set_params, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -18,9 +18,15 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+  end
 
-  def edit; end
+  def edit
+  end
+
+  def destroy
+    
+  end
 
   def update
     if @item.update(items_params)

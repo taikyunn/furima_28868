b13f3a9591385696d84bd4fb671ac5180d97ed61
taikyunn <taichi_141@me.com>
2020-09-07 13:37:43 +0900

    マイグレーション完了

--- db/migrate/20200907042308_create_places.rb
+++ db/migrate/20200907042308_create_places.rb
@@ -1,11 +1,14 @@
 class CreatePlaces < ActiveRecord::Migration[6.0]
   def change
     create_table :places do |t|
-
-      t.timestamps
       t.integer :prefecture_id, null: false
       t.string :postal_code, null: false
       t.string :city, null: false
+      t.string :address, null: false
+      t.string :buinding
+      t.string :phone_number, null: false
+      t.integer :purchase_id, null: false, foreign_key: true
+      t.timestamps
     end
   end
 end

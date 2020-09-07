0449e2c8b0732b8c42d485679f731e31bc9477fa
taikyunn <taichi_141@me.com>
2020-09-07 13:44:29 +0900

    prefecture_idモデル作成

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  
   belongs_to :purchase
 
   with_options precense: true do

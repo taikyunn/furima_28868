fae771fca7d990aec32a50418991fec318f61444
taikyunn <taichi_141@me.com>
2020-09-08 18:31:23 +0900

    factorybot完成

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :place do
-    
+    prefecture_id {"3"}
+    postal_code   {"123-4567"}
+    city          {"港区六本木"}
+    address       {"麻布1-1-1"}
+    building      {"六本木ヒルズ"}
+    phone_number  {"08011111111"}
+    purchase_id   {"1"}
+    association :purchase
   end
 end

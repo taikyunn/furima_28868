0c38b9b468705fc85a3253ddd1c3301b2740639a
taikyunn <taichi_141@me.com>
2020-09-08 19:35:55 +0900

    テストコード　it完了

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -3,48 +3,47 @@ require 'rails_helper'
 RSpec.describe Place, type: :model do
   describe Place do
     before do
+      @user = FactoryBot.create(:user)
+      @item = FactoryBot.create(:item)
+      @place = FactoryBot.build(:place,user_id:@user.id, item_id: @item.id)
     end
   end
 
   describe '商品購入機能'do
     context '商品購入が上手くいく時'do
-      
+    it '全てが上手くいくと商品購入できる' do
+      expect(@place).to be_valid
+    end
     end
 
-    context '商品出品が上手くいかない時' do
+    context '商品購入が上手くいかない時' do
       
     end
-    it do
+    it 'prefecture_idが空だと購入できない' do
       
     end
-    it do
+    it 'postal_codeが空だと購入できない' do
       
     end
-    it do
+    it 'postal_codeにハイフンがないと購入できない' do
       
     end
-    it do
+    it 'cityが空だと購入できない' do
       
     end
-    it do
+    it 'addressが空だと購入できない' do
       
     end
-    it do
+    it 'phone_numberが空だと購入できない' do
       
     end
-    it do
+    it 'phone_numberが11桁より多いと購入できない' do
       
-    end
-    it do
+  end
+  it 'phone_numberが空だと購入できない' do
       
-    end
-    it do
-      
-    end
-    it do
-      
-    end
-    it do
+end
+    it 'purchase_idが空だと購入できない'do
       
     end
   end

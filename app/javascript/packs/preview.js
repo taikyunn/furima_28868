a9787f99e4c7f1c91634e19e994969e6c340f1f4
taikyunn <taichi_141@me.com>
2020-09-16 08:41:42 +0900

    プレビュー機能完成

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
-  const ImageList = document.getElementById('item-image');
+  const ImageList = document.getElementById('image-list');
   document.getElementById('item-image').addEventListener('change', function(e){
-    console.log(123);
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const imageElement = document.createElement('div');
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('src',blob);
+
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
   });
 });

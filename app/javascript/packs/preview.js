53cee6304bc53a7a235f1351e51ea264edb35b9c
taikyunn <taichi_141@me.com>
2020-09-15 19:32:28 +0900

    発火完了

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const ImageList = document.getElementById('item-image');
+  document.getElementById('item-image').addEventListener('change', function(e){
+    console.log(123);
+  });
+});

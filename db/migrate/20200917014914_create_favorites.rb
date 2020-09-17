f95b3a07abdaf9201c3c34b516e6a6266c2d5789
taikyunn <taichi_141@me.com>
2020-09-17 10:54:27 +0900

    マイグレート

--- db/migrate/20200917014914_create_favorites.rb
+++ db/migrate/20200917014914_create_favorites.rb
@@ -1,7 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[6.0]
   def change
     create_table :favorites do |t|
-
+      t.integer :item_id, null: false, foreign_key: true
+      t.integer :user_id, null: false, foreign_key: true
       t.timestamps
     end
   end
